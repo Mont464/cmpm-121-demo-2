218e2d3b2c86d1030b9a9d8963f8b310a387f605
Mont778 <alejandro.montoreano@gmail.com>
2024-10-20 23:00:10 -0700

    Step 2 possibly Done

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,44 @@ header.innerHTML = APP_NAME;
 app.append(header);
 
 const canvas = document.createElement("canvas");
-canvas.width = 256;
-canvas.height = 256;
 app.append(canvas);
+
+const canvCont = canvas.getContext("2d");
+
+let mouseX = 0;
+let mouseY = 0;
+let mouseActive = false;
+
+canvas.addEventListener("mousedown", (ev) => {
+    mouseX = ev.offsetX;
+    mouseY = ev.offsetY;
+    mouseActive = true;
+});
+
+canvas.addEventListener("mousemove", (ev) => {
+    if (mouseActive) {
+        canvCont?.beginPath();
+        canvCont?.moveTo(mouseX, mouseY);
+        canvCont?.lineTo(ev.offsetX, ev.offsetY);
+        canvCont?.stroke();
+        canvCont?.closePath();
+        mouseX = ev.offsetX;
+        mouseY = ev.offsetY;
+    }
+});
+
+canvas.addEventListener("mouseup", () => {
+    mouseX = 0;
+    mouseY = 0;
+    mouseActive = false;
+});
+
+
+
+const cButton = document.createElement("button");
+cButton.innerHTML = "Clear Drawing";
+app.append(cButton);
+
+cButton.onclick = () => {
+    canvCont?.clearRect(0, 0, canvas.width, canvas.height);
+};
